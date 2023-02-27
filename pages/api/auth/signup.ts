af01bef1544535e41e831b19aacfaacae2f4b82f
AhmedQeshta <ahmedqeshta1999@gmail.com>
2023-02-27 13:45:29 +0200

    add send enail and congiration register new email in db

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,8 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { hash } from 'bcryptjs';
-import connectMongo from '@/database/connection';
-import Users from '@/models/UsersSchema';
+import connectMongo from '@/lib/mongodb';
+import Users from '@/utils/models/UsersSchema';
+import { sendEmail } from '@/lib/email';
+import { newUserEmail } from '@/utils/emailTemplets';
 
 type userType = {
   username: string;
@@ -38,5 +40,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const user = await Users.create({ username, email, password: hashedPassword });
 
   if (!user) return res.status(500).json({ status: false, message: 'Internal Server Error' });
+
+  sendEmail({
+    to: email ?? '',
+    subject: 'Welcome to NextAPI',
+    html: newUserEmail(email ?? '', `${process.env.NEXT_PUBLIC_LOCAL_AUTH_URL}/login`, password),
+  });
+
   return res.status(200).json({ status: true, message: 'user register successfully', user });
 }
